bca0376b60fd0a2724a7b68a3dcbbf709fd96e9d
yGKeKe <mlord@widener.edu>
2022-04-17 18:11:39 -0400

    Initial commit

---
+++ Dictionary.java
@@ -0,0 +1,9 @@
+import java.util.Hashmap;
+import java.util.Map;
+
+public class Dictionary {
+    public static void main(String[] args){
+        Map<String, String> koreanToEnglishDictionary = new HashMap<String, String>();
+
+    }
+}
